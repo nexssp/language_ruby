5faff54032708f4e88c1f87f1b97323008276786
Marcin Polak <mapoart@gmail.com>
2019-12-14 09:34:34 +0100

    Added ocra as builder (not working yet fully - to check)

--- ruby.win32.nexss.config.js
+++ ruby.win32.nexss.config.js
@@ -11,24 +11,19 @@ languageConfig.description =
 languageConfig.url = "https://www.ruby-lang.org";
 languageConfig.extensions = [".rb"];
 languageConfig.builders = {
-  tofill: {
-    install: "scoop install llvm",
-    //build: "pkg --output <destinationFile> --out-path <destinationPath> <file>",
-    command: "clang++",
-    build: function() {
-      const path = require("path");
-      //take command from current folder
-      return path.join(__dirname, "customCompiler.win32.ruby.cmd");
-    },
-    args: "<file> <destinationFile>",
-    // C++ needs to be build to exe, so no compile option
-    help: ``
-  }
+  // ocra: {
+  //   install: "gem install ocra",
+  //   command: "ocra",
+  //   build: function() {
+  //     return "ocra";
+  //   },
+  //   args: "<file> --output <destinationFile>",
+  //   help: ``
+  //}
 };
 languageConfig.compilers = {
   ruby: {
     install: "scoop install ruby",
-    // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
     command: "ruby",
     args: "-EUTF-8 <file>",
     help: ``
@@ -38,18 +33,17 @@ languageConfig.errors = require("./nexss.ruby.errors");
 languageConfig.languagePackageManagers = {
   gem: {
     installation: "installed.",
-    messageAfterInstallation:
-      "Add to the top of your php file(s): require __DIR__ . '/vendor/autoload.php';", //this message will be displayed after this package manager installation, maybe some action needed etc.
-    installed: "gem installed <args>",
-    search: "gem search <args>",
-    install: "gem install <args>",
-    uninstall: "gem remove <args>",
-    help: "gem <args>",
+    messageAfterInstallation: "",
+    installed: "gem installed",
+    search: "gem search",
+    install: "gem install",
+    uninstall: "gem remove",
+    help: "gem",
     version: "gem version",
     init: () => {},
     // if command not found in specification
     // run directly on package manager
-    else: "gem <default> <args>"
+    else: "gem"
   }
 };
 
